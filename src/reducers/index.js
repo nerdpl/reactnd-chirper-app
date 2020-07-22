35de51f3f449ed5ae3baf3b1c1e84c386df1a182
Andrzej Sobczyk <nooneeverreallydies@tlen.pl>
2020-07-22 12:48:41 +0200

    Tweet component done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux'
-
+import { loadingBarReducer } from 'react-redux-loading'
 import users from './users'
 import tweets from './tweets'
 import authedUser from './authedUser'
 
 export default combineReducers({
+  authedUser,
   users,
   tweets,
-  authedUser
+  loadingBar: loadingBarReducer
 })
